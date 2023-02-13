6df7969b06f708a33471e3f05c24ab7ac01dfdda
Karan Kumar Goyal <123921103+karangoyal02@users.noreply.github.com>
2023-02-13 12:15:38 +0530

    Add files via upload

---
+++ assignment 3/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    //for append
+    $(".btn").click(function(){
+    $(".1").appendTo(".container2");
+    $(".btn").click(function(){
+    $(".2").appendTo(".container2"); 
+    $(".btn").click(function(){
+    $(".3").appendTo(".container2"); 
+    $(".btn").click(function(){
+    $(".4").appendTo(".container2");
+    $(".btn").click(function(){
+    $(".5").appendTo(".container2");
+    $(".btn").click(function(){
+    $(".6").appendTo(".container2");
+    $(".btn").click(function(){
+    $(".7").appendTo(".container2");
+    $(".btn").click(function(){
+    $(".8").appendTo(".container2"); 
+    $(".btn").click(function(){
+    $(".9").appendTo(".container2"); 
+    $(".btn").click(function(){
+    $(".10").appendTo(".container2"); 
+    });});}); }); }); }); }); });  });  }); 
+
+    //for prepend
+    $(".btn2").click(function(){
+    $(".1").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".2").prependTo(".container1"); 
+    $(".btn2").click(function(){
+    $(".3").prependTo(".container1"); 
+    $(".btn2").click(function(){
+    $(".4").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".5").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".6").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".7").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".8").prependTo(".container1");
+    $(".btn2").click(function(){
+    $(".9").prependTo(".container1");       
+    $(".btn2").click(function(){
+    $(".10").prependTo(".container1");       
+    }); });      }); }); }); }); }); });  });  }); 
+})
